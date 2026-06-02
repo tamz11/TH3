6e60359689f48ed4d0c5144ffc7f9aa4deb0987b
tamz11 <mintam212005@gmail.com>
2026-06-02 13:23:18 +0700

    fix bug

--- gcp-photo-gallery/frontend/src/Dashboard.jsx
+++ gcp-photo-gallery/frontend/src/Dashboard.jsx
@@ -468,6 +468,15 @@ export default function Dashboard() {
               </div>
             </div>
             )}
+            
+          </div>
+        </main>
+      </div>
+
+      {/* PHOTO MODAL */}
+      {selectedPhoto && (
+        <div className="fixed inset-0 z-50 flex items-center justify-center p-4 sm:p-6 bg-gray-900/80 backdrop-blur-sm">
+          <div className="bg-white w-full max-w-6xl max-h-[90vh] rounded-2xl md:rounded-[2rem] overflow-hidden shadow-2xl flex flex-col md:flex-row relative">
             <button 
               onClick={() => setSelectedPhoto(null)}
               className="absolute top-4 right-4 md:top-6 md:right-6 z-20 p-2 bg-gray-900/40 hover:bg-gray-900/80 backdrop-blur-md text-white rounded-full transition-colors"
